2dad578026dcb003e15e896556825a8eb2e17d86
Maroussia Bednarkiewicz <maroussia.b at gmail.com>
2019-01-29 22:50:38 +0100

    Added support to color the nodes according to origin, documented the addition and provided an updated example.

--- graph.py
+++ graph.py
@@ -2,17 +2,26 @@
    the transmitters and the transmission paths
    together with its subgraph (timeline)."""
 
-from graphviz import Digraph
+from collections import defaultdict
+import itertools
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from graphviz import Digraph
+
 from ranking import rank_list, same_rank_subgraph, a_timeline
 
 
-def draw_graph(path_to_transmitters_file, path_to_transmissions_file, timeline_step):
+def draw_graph(path_to_transmitters_file,
+               path_to_transmissions_file,
+               timeline_step, color_origin=dict()):
     """Creates a graph with transmitters as nodes and transmission paths as edges.
        The following parameters must be provided:
        - a path to the csv file containing the transmitters
        - a path to the csv file containing the transmission
        - the time lapses in years that represent the timeline nodes (timeline_step)
+       - (optional) a dictionary specifying colors for some or all of
+         the origins, or 'auto' for automatic color assignment
     """
     
     assert path_to_transmissions_file.endswith('.csv') and\
@@ -31,9 +40,24 @@ def draw_graph(path_to_transmitters_file, path_to_transmissions_file, timeline_s
                 ranksep='1.2', concentrate='false'),
                 node_attr=dict(shape='none'))
     
-    
+
+    if color_origin=='auto':
+        cmap = plt.cm.get_cmap('tab20')
+        colors = (mpl.colors.to_hex(cmap(i)) for i in range(20))
+        color_cycle = itertools.cycle(colors)
+        # Matching colors to origins
+        color_origin = dict()
+        for i, origin in enumerate(df_nodes.Origin.unique()):
+            color_origin[origin] = next(color_cycle)   
+    else:
+        # every origin black except those in the color_origin parameter
+        color_origin = defaultdict(lambda: 'black', color_origin)
+
+    df_nodes['Color'] = df_nodes.Origin.map(color_origin)
+        
+    # Creating the nodes
     for _, row in df_nodes.iterrows():
-        g.node(name=row.Transmitters, fontcolor='black')
+        g.node(name=row.Transmitters, fontcolor=row.Color)
 
     # Defining the timeline subgraph attributes
     minimum = df_nodes.Ranking.min()
@@ -53,21 +77,25 @@ def draw_graph(path_to_transmitters_file, path_to_transmissions_file, timeline_s
         nodes_ranks = rank_list(str(rank*timeline_step), rank, df_nodes)
         same_rank_subgraph(g, nodes_ranks)
 
-    # Drawing the edges (transmission paths)
+    # Creating the edges (transmission paths)
     for _, row in df_edges.iterrows():
         g.edge(row.From, row.To, arrowhead='vee')
 
     return g
 
-def view_graph(path_to_transmitters_file, path_to_transmissions_file, timeline_step, graph_format):
+def view_graph(path_to_transmitters_file,
+               path_to_transmissions_file,
+               timeline_step, color_origin=dict(),  graph_format='pdf'):
     """Displays the final graph (isnad tree) in the given format;
        The following parameters must be provided:
        - a path to the csv file containing the transmitters
        - a path to the csv file containing the transmission
        - the time lapses in years that represent the timeline nodes (timeline_step)
-       - the export format: 'svg', 'pdf', 'png'
+       - the export format: default 'pdf'; others: 'svg', 'png'
+       - (optional) a dictionary specifying colors for some or all of
+         the origins, or 'auto' for automatic color assignment
     """
-    g = draw_graph(path_to_transmitters_file, path_to_transmissions_file, timeline_step)
+    g = draw_graph(path_to_transmitters_file, path_to_transmissions_file, timeline_step, color_origin=color_origin)
     g.format=graph_format
 
     g.view()
