b0a7ca5a9e047174dde8b11047dbe98a48bdb772
Maroussia <maroussia.b@gmail.com>
2022-11-22 21:15:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-	name='isnalyser', # what people pip install
-	version='0.1.0', # version of our library
+	name='isnalyser',
+	version='0.1.0',
 	description='Create isnad graphs automatically.',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	packages=setuptools.find_packages(),
 	python_requires=">=3.5", # python version
 	install_requires= ["pandas>=0.24.2", "graphviz>=0.11.1", "matplotlib>=2.0.0", "numpy>=1.12.0"], # dependencies and their versions
-	url='https://github.com/dhakara/isnalyser',
+	url='https://github.com/dhakarat/isnalyser',
 	author='Maroussia Bednarkiewicz, Stefan Wezel, Álvaro Tejero Cantero',
 	author_email='maroussia.b@gmail.com',
 	include_package_data=True,
